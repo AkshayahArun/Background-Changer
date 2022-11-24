b66669c2723c04eb6f37d041628447f3edc1f9ac
AkshayahArun <118975057+AkshayahArun@users.noreply.github.com>
2022-11-24 18:30:56 +0800

    Add files via upload
    Byju Coding

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+var box
+
+function preload(){
+
+}
+
+function setup() {
+createCanvas(800,800)
+box=createSprite(350,350,100,100)
+}
+
+function draw() {
+background("blue")
+drawSprites();
+}
+
+
